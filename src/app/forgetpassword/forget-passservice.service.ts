0aab765f04ad42c5685cca23a2bdd44c595a22a7
mdlayeeque urrehman <mdlayeeque.urrehman@ATMECS.CORP>
2018-09-06 13:19:35 +0530

    changes

--- src/app/forgetpassword/forget-passservice.service.ts
+++ src/app/forgetpassword/forget-passservice.service.ts
@@ -11,8 +11,8 @@ export class ForgetPassserviceService {
     console.log("in service ")
 
     // return this.http.post("http://localhost:5000/login", { "name": ename,"password": password }, {})
-   // return this.http.post("https://mytestapp128798.herokuapp.com/changePassword", { "email": email }, {})
-   return this.http.post("http://localhost:5000/changePassword", { "email": email }, {})
+   return this.http.post("https://mytestapp128798.herokuapp.com/changePassword", { "email": email }, {})
+   //return this.http.post("http://localhost:5000/changePassword", { "email": email }, {})
 
       .pipe(map(res => res.json()));
   }
